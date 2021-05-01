c680e315dbcd6c466e16bc1dcd25e1913eb66907
pramod <kumarpramod66476@gmail.com>
2021-05-01 13:48:55 +0530

    ok

--- type-o-server/index.js
+++ type-o-server/index.js
@@ -1,7 +1,33 @@
+const ex = require('express');
 const readline = require('readline');
+const event = require('events');
 const rl = readline.createInterface({input: process.stdin});
+const app = ex();
 let input;
-rl.on('line',(entered) => {
-    input = entered;
-    console.log(input)
+const emitter = new event();
+
+
+
+
+
+
+
+
+
+const port = 9999;
+app.get('/', (req, res) => {
+    if(req){
+        (function(){ rl.on('line',(line) => {
+            input = line;
+            emitter.emit('typed');
+        })
+        pk = () => {res.end(input)};
+        emitter.on('typed',()=>{
+            pk();
+        });}())
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
